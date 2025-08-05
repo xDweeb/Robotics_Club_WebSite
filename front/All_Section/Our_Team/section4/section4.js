c388e4013df00b17fc8efee2415db38f48dbfecb
Yaser-Yjj <yaseryjjou@gmail.com>
2025-08-05 20:26:03 +0100

    Chore : Setup the projet

--- front/All_Section/Our_Team/section4/section4.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let carousel = new bootstrap.Carousel(document.querySelector('#teamCarousel'), {
-        interval: 3000, 
-        ride: 'carousel'
-    });
-});
